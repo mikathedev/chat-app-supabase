635415183eeaa1eabf64015b42b60dc428f85d40
mikathedev <mikathedev@proton.me>
2023-12-17 17:57:18 -0500

    other

--- src/App.js
+++ src/App.js
@@ -10,44 +10,57 @@ let user = await supabase.auth.getUser().then(
 function App() {
   return (
       <section>
-        {user.user ? <SignIn/> : <SignIn/> }
+        {user ? <SignIn/> : <SignIn/> }
       </section>
   );
 
 }
 
 function SignIn() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const changeEmail = (e) => setEmail(e.target.value)
-  const changePassword = (e) => setPassword(e.target.value)
-  return(
-      <div>
-        <form>
-          <label>Email</label>
-          <input value={email} onChange={changeEmail} type="email" name="email"/>
-          <label>Password</label>
-          <input value={password} onChange={changePassword} type="password" name="password"/>
-          <button onClick={(email, password) =>
-          supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
-          }).then(r => console.log("signed in", r))
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const changeEmail = (e) => setEmail(e.target.value)
+    const changePassword = (e) => setPassword(e.target.value)
 
-          }>Sign In</button>
-        </form>
-      </div>
+  return(
+      <section className={"signin"}>
+          <form>
+              <label>Email</label>
+              <input value={email} onChange={changeEmail} type="email" name="email"/>
+              <label>Password</label>
+              <input value={password} onChange={changePassword} type="password" name="password"/>
+              <button type={"button"} onClick={signin(email, password)}>Sign In
+              </button>
+              <button type={"button"} onClick={signup(email, password)}>Sign up</button>
+          </form>
+      </section>
   )
 }
 
 
-
 function Chatroom() {
-  return (
-    <div>
-      <h1>Chatroom</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Chatroom</h1>
+        </div>
+    );
+}
+
+function signin(email, pass) {
+    supabase.auth.signInWithPassword({
+        email: email,
+        password: pass,
+    }).then(r => {
+        return console.log("signed up", r)
+    })
+}
+function signup(email, pass) {
+    supabase.auth.signUp({
+        email: email,
+        password: pass,
+    }).then(r => {
+        return console.log("signed up", r)
+    })
 }
 
 export default App;
