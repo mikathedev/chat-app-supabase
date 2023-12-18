3bfcf4aaa3a7f8756cd42c3f70f929abc9f3ce57
mikathedev <mikathedev@proton.me>
2023-12-17 20:47:12 -0500

    other

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
+import { useState } from'react';
 
 const supabaseUrl = 'https://wvpnvgvxtecukdoxmbad.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2cG52Z3Z4dGVjdWtkb3htYmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4NDQ0NjcsImV4cCI6MjAxODQyMDQ2N30.JQuhGVAqT1OeSHJAbmECj5Q8iwTqJ4ebYEoeFdsNQb8"
@@ -9,22 +10,53 @@ let user = await supabase.auth.getUser().then(
 function App() {
   return (
       <section>
-        {user ? <Chatroom/> : <SignIn/> }
+        {user ? <SignIn/> : <SignIn/> }
       </section>
   );
 
 }
 
-function SignIn() {
+async function SignIn(credentials) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const changeEmail = (e) => setEmail(e.target.value)
+    const changePassword = (e) => setPassword(e.target.value)
 
-  return(
-      <section className={"signin"}>
-          <form>
-              <button type={"button"} onClick={supabase.auth.signInWithOAuth({provider:"google"})}>Sign in with supabase</button>
-          </form>
-      </section>
-  )
+
+    return (
+        <section className={"signin"}>
+            <form>
+                <label>Email</label>
+                <input value={email} onChange={changeEmail} type="email" name="email"/>
+                <label>Password</label>
+                <input value={password} onChange={changePassword} type="password" name="password"/>
+                <button type={"button"} onClick={signInWithEmail(email, password)}>Sign In
+                </button>
+                <button type={"button"} onClick={await supabase.auth.signInWithPassword(email, password)}>Sign in with
+                    supabase
+                </button>
+            </form>
+        </section>
+    )
 }
+
+async function signInWithEmail(email, pass) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: pass
+    })
+}
+
+async function signUpNewUser(email, password) {
+    const { data, error } = await supabase.auth.signUp({
+        email: 'example@email.com',
+        password: 'example-password',
+        options: {
+            emailRedirectTo: 'https//example.com/welcome'
+        }
+    })
+}
+
 
 
 function Chatroom() {
