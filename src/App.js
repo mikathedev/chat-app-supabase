4a61cd16a2782ba7048be5b45d558d8190d66460
mikathedev <mikathedev@proton.me>
2023-12-19 17:27:52 -0500

    other

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import { useState } from'react';
 const supabaseUrl = 'https://wvpnvgvxtecukdoxmbad.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2cG52Z3Z4dGVjdWtkb3htYmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4NDQ0NjcsImV4cCI6MjAxODQyMDQ2N30.JQuhGVAqT1OeSHJAbmECj5Q8iwTqJ4ebYEoeFdsNQb8"
 const supabase = createClient(supabaseUrl, supabaseKey)
-let user = await supabase.auth.getUser().then(
-    r => console.log("user", r))
+const user = await supabase.auth.getUser()
 function App() {
   return (
       <section>
-        {user ? <SignIn/> : <SignIn/> }
+        {user.data.user ? <Chatroom/> : <SignIn/> }
       </section>
   );
 
 }
 
-async function SignIn(credentials) {
+function SignIn() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const changeEmail = (e) => setEmail(e.target.value)
@@ -30,11 +29,8 @@ async function SignIn(credentials) {
                 <input value={email} onChange={changeEmail} type="email" name="email"/>
                 <label>Password</label>
                 <input value={password} onChange={changePassword} type="password" name="password"/>
-                <button type={"button"} onClick={signInWithEmail(email, password)}>Sign In
-                </button>
-                <button type={"button"} onClick={await supabase.auth.signInWithPassword(email, password)}>Sign in with
-                    supabase
-                </button>
+                <button type={"button"} onClick={() => signInWithEmail(email, password)}>Sign In</button>
+                <button type={"button"} onClick={() => signUpNewUser(email, password)}>Sign up with supabase</button>
             </form>
         </section>
     )
@@ -45,16 +41,23 @@ async function signInWithEmail(email, pass) {
         email: email,
         password: pass
     })
+    console.log("data", data, "error", error)
+    // eslint-disable-next-line no-restricted-globals
+    location.reload()
 }
 
 async function signUpNewUser(email, password) {
     const { data, error } = await supabase.auth.signUp({
-        email: 'example@email.com',
-        password: 'example-password',
+        email: email,
+        password: password,
         options: {
-            emailRedirectTo: 'https//example.com/welcome'
+            emailRedirectTo: 'mikachat.netify.com'
         }
-    })
+    });
+    // eslint-disable-next-line no-restricted-globals
+    location.reload()
+
+    console.log("data", data, "error", error)
 }
 
 
@@ -63,8 +66,17 @@ function Chatroom() {
     return (
         <div>
             <h1>Chatroom</h1>
+            <button onClick={() => signOutUser()}>sign out</button>
         </div>
     );
+
+}
+
+async function signOutUser() {
+    const { error } = await supabase.auth.signOut()
+    console.log("error", error)
+    // eslint-disable-next-line no-restricted-globals
+    location.reload()
 }
 
 export default App;
